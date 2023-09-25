29cfc7bff68c8390dfa4449dbb7ea9b5b25262b0
Iara <iara.vitabarhuber@gmail.com>
2023-09-25 17:45:12 -0300

    Program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -11,14 +11,19 @@ namespace Program
             //Creamos personaje 1:
             Personaje mago1 = new Personaje ("Mago", "Voldemort", true);
             //Creamos personaje 2:
-
+            Personaje elfo1 = new Personaje ("Elfo", "Harrius", true);
             //Items de personaje 1:
             ConstructorItem bastonmagico = new ConstructorItem ("Bastón Mágico", 10, 0);
             ConstructorItem tunica = new ConstructorItem ("Túnica", 5, 0);
+            //Items de personaje 2:
+            ConstructorItem hachaMágica = new ConstructorItem ("Hacha Mágica", 5, 10);
+            ConstructorItem capa = new ConstructorItem ("capa", 10, 10);
 
             //Equipamos ítems: 
             bastonmagico.EquiparItem(mago1);
             tunica.EquiparItem(mago1);
+            hachaMágica.EquiparItem(elfo1);
+            capa.EquiparItem(elfo1);
 
             //Batalla:
            /* Batalla.CalcularResultadoBatalla(mago1, personaje2);
